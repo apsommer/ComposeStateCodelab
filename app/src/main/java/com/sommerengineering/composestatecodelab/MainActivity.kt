96c8ce49263143d390ae483649b0bfe204701aef
Drew Sommer <drew@sommerengineering.com>
2024-09-21 12:54:42 +0300

    feat: Create basic water counter

--- app/src/main/java/com/sommerengineering/composestatecodelab/MainActivity.kt
+++ app/src/main/java/com/sommerengineering/composestatecodelab/MainActivity.kt
@@ -34,8 +34,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
-    )
+        modifier = modifier)
 }
 
 @Preview(showBackground = true)
