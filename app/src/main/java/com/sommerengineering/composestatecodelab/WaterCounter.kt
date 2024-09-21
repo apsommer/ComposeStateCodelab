16362c1339379378346c9d43cfeebf3aac8ec02d
Drew Sommer <drew@sommerengineering.com>
2024-09-21 21:20:04 +0300

    feat: Show text and toggle button state

--- app/src/main/java/com/sommerengineering/composestatecodelab/WaterCounter.kt
+++ app/src/main/java/com/sommerengineering/composestatecodelab/WaterCounter.kt
@@ -1,15 +1,38 @@
 package com.sommerengineering.composestatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp))
+
+    // MutableState is automatically tracked by compose
+    // remember inline function is similar to private val
+    // 'by' keyword defines 'delegate' allowing indirect reference to getter and setter, reduces code
+    var count by remember { mutableIntStateOf(0) }
+
+    Column(modifier = modifier.padding(16.dp)) {
+
+        if (count > 0) {
+            Text(
+                text = "You've had $count glasses.",
+                modifier = modifier.padding(16.dp))
+        }
+        Button(
+            enabled = count < 10,
+            onClick = { count ++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
 }
