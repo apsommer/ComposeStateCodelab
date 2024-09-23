63ef9548e589887e9dc1c31adfbc82bcb43ce224
Drew Sommer <drew@sommerengineering.com>
2024-09-23 11:46:02 +0300

    refactor: Revert to simplified ui

--- app/src/main/java/com/sommerengineering/composestatecodelab/WaterCounter.kt
+++ app/src/main/java/com/sommerengineering/composestatecodelab/WaterCounter.kt
@@ -16,36 +16,20 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
 
-    // MutableState is automatically tracked by compose
-    // remember inline function is similar to private val
-    // 'by' keyword defines 'delegate' allowing indirect reference to getter and setter, reduces code
-    var count by remember { mutableStateOf(0) }
-
     Column(modifier = modifier.padding(16.dp)) {
 
-        if (count > 0) {
+        // MutableState is automatically tracked by compose
+        // remember inline function is similar to private val
+        // 'by' keyword defines 'delegate' allowing indirect reference to getter and setter, reduces code
+        var count by remember { mutableStateOf(0) }
 
-            // show task is forgotten when count = 0 as this line is not called
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 minute walk today?",
-                    onClose = { showTask = false }
-                )
-            }
+        if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(
-                enabled = count < 10,
-                onClick = { count ++ }) {
-                Text(text = "Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 8.dp)) {
-                Text(text = "Clear water counter")
-            }
+        Button(
+            enabled = count < 10,
+            onClick = { count ++ }) {
+            Text(text = "Add one")
         }
     }
 }
