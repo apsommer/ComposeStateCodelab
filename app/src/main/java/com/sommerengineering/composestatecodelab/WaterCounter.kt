36987b0e84ed82ddf61c1997829f2a832d8b24b4
Drew Sommer <drew@sommerengineering.com>
2024-09-23 14:18:11 +0300

    refactor: Hoist state from ui composeable

--- app/src/main/java/com/sommerengineering/composestatecodelab/WaterCounter.kt
+++ app/src/main/java/com/sommerengineering/composestatecodelab/WaterCounter.kt
@@ -1,34 +1,44 @@
 package com.sommerengineering.composestatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier = Modifier) {
 
+    // MutableState is automatically tracked by compose
+    // remember inline function is similar to private val
+    // rememberSaveable retains state across configuration changes
+    // 'by' keyword defines 'delegate' allowing indirect reference to getter and setter, reduces code
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(
+        count,
+        { count++ },
+        modifier)
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier = modifier.padding(16.dp)) {
-
-        // MutableState is automatically tracked by compose
-        // remember inline function is similar to private val
-        // 'by' keyword defines 'delegate' allowing indirect reference to getter and setter, reduces code
-        var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
             Text("You've had $count glasses.")
         }
         Button(
             enabled = count < 10,
-            onClick = { count ++ }) {
+            onClick = onIncrement) {
             Text(text = "Add one")
         }
     }
