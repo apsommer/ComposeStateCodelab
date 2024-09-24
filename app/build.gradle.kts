630549f000aaa0115a5f48f2873b052e8e24f22d
Drew Sommer <drew@sommerengineering.com>
2024-09-24 18:34:17 +0300

    refactor: Use latest naming convention

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,5 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:{latest_version}")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
